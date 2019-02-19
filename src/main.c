8720fe8b07c7c25636bf39ae951a534dbe3a6b6f
Герасимов Сергей Дмитриевич <is741s05@bwp01.csc.local>
2019-02-19 11:23:27 +0700

    add func

--- src/main.c
+++ src/main.c
@@ -1,5 +1,32 @@
 #include "mySimpleComputer.h"
 
+void check_err(int check)
+{
+	if (check == ERROR)
+		printf("ERROR: array out of order\n");
+}
+
 int main()
 {
+	int check;
+	check = sc_memoryInit();
+	check_err(check);
+		
+	int index, val;
+	printf("Enter index: ");
+	scanf("%d", &index);
+	
+	printf("Enter number: ");
+	scanf("%d", &val);
+	
+	check = sc_memorySet(index, val);
+	check_err(check);
+	
+	check = sc_memoryGet(0, &val);
+	check_err(check);
+	
+	printf("%d \n", val);
+	
+	return 0;
+}
 	
--- src/mySimpleComputer.c
+++ src/mySimpleComputer.c
@@ -1,3 +1,31 @@
 #include "mySimpleComputer.h"
 
-int sc_memoryInit();
+int sc_memoryInit()
+{
+	for (int i = 0; i < N; i++){
+		if (&arr[i] == NULL) return ERROR;
+		arr[i] = 0;
+	}
+	return OK;
+}
+
+int sc_memorySet(int adress, int value)
+{
+	if (adress >= N || adress < 0)
+		return ERROR;
+	else
+		arr[adress] = value;
+		
+	return OK;
+}
+
+int sc_memoryGet(int adress, int *value)
+{
+	if (adress >= N || adress < 0)
+		return ERROR;
+	else
+		*value = arr[adress];
+		
+	return OK;
+}
+
