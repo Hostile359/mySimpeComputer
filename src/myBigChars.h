d7d474b84a963cc0a81e76d3cbd4c0b68d5fadfd
admin <admin@localhost.localdomain>
2019-04-29 16:13:07 +0700

    add new big char numbers

--- src/myBigChars.h
+++ src/myBigChars.h
@@ -14,7 +14,25 @@ enum BigChar {
 	plus_top = 4279769112,
 	plus_bot = 404232447,
 	one_top = 404626456,
-	one_bot = 4279769112
+	one_bot = 4279769112,
+	two_top = 3233858559,
+	two_bot = 4278387711,
+	three_top = 4173381887,
+	three_bot = 4290822336,
+	four_top = 4291019715,
+	four_bot = 3233857728,
+	five_top = 4278387711,
+	five_bot = 4290822336,
+	six_top = 4278387711,
+	six_bot = 4291019715,
+	seven_top = 3233857791,
+	seven_bot = 3233857728,
+	eight_top = 4291019775,
+	eight_bot = 4291019715,
+	nine_top = 4291019775,
+	nine_bot = 4290822336,
+	zero_top = 3284386815,
+	zero_bot = 4291019715
 };
 
 int bc_printA (char *str);
